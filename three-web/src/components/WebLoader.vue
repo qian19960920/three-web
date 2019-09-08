839a950f4d85e86c0e601e81aef6f2ce49e853a0
zhangyaxian <799557171@qq.com>
2019-09-08 21:02:20 +0800

    简单框架

---
+++ three-web/src/components/WebLoader.vue
@@ -0,0 +1,16 @@
+<template>
+	<div id="container">111</div>
+</template>
+
+<script>
+export default {
+  name: 'WebLoader',
+  data(){
+
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- three-web/src/views/Home.vue
+++ three-web/src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WebLoader/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import WebLoader from '@/components/WebLoader.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    WebLoader
   }
 }
 </script>
